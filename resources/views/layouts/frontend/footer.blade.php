1a7b9a389a54eee865d129690fed63757f93d2e1
CristianoAlpha <cristianofandula07@gmail.com>
2025-03-30 14:11:51 +0100

    pequenas mudanças no front

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -1,4 +1,4 @@
-<!--footer start-->
+<!-- Início do footer -->
 <footer class="footer widget-footer clearfix">
     <div class="first-footer">
         <div class="container">
@@ -6,17 +6,17 @@
                 <div class="col-lg-12 text-center">
                     <div class="first-footer-inner">
                         <div class="footer-logo">
-                            <img id="footer-logo-img" class="img-center" src="images/footer-logo.png" alt="">
+                            <img id="footer-logo-img" class="img-center" src="{{ asset('frontend/images/footer-logo.png') }}" alt="">
                         </div>
                         <div class="row no-gutters footer-box">
                             <div class="col-md-4 widget-area">
                                 <div class="featured-box text-center">
                                     <div class="featured-content">
                                         <div class="featured-title">
-                                            <h5>Our Address</h5>
+                                            <h5>Nosso Endereço</h5>
                                         </div>
                                         <div class="featured-desc">
-                                            <p>Evanto HQ 24 Fifth st., Los Angeles, USA</p>
+                                            <p>Cidade Alta I</p>
                                         </div>
                                     </div>
                                 </div>
@@ -25,10 +25,10 @@
                                 <div class="featured-box text-center">
                                     <div class="featured-content">
                                         <div class="featured-title">
-                                            <h5>Talk To Expert</h5>
+                                            <h5>Fale com o Especialista</h5>
                                         </div>
                                         <div class="featured-desc">
-                                            <p>+1234 567 8910 or +1234 567 8911</p>
+                                            <p>+1234 567 8910 ou +1234 567 8911</p>
                                         </div>
                                     </div>
                                 </div>
@@ -37,7 +37,7 @@
                                 <div class="featured-box text-center">
                                     <div class="featured-content">
                                         <div class="featured-title">
-                                            <h5>Email Us</h5>
+                                            <h5>Envie-nos um E-mail</h5>
                                         </div>
                                         <div class="featured-desc">
                                             <p>example@yourdomain.com</p>
@@ -49,12 +49,12 @@
                         <div class="row no-gutters">
                             <div class="col-md-3 col-sm-2"></div>
                             <div class="col-md-6 col-sm-8">
-                                <div class="">
+                                <div>
                                     <form id="subscribe-form" class="newsletter-form" method="post" action="#" data-mailchimp="true">
                                         <div class="mailchimp-inputbox clearfix" id="subscribe-content">
                                             <i class="fa fa-envelope-o"></i>
-                                            <input type="email" name="email" placeholder="Your Email Address.." required="">
-                                            <input type="submit" value="SUBMIT">
+                                            <input type="email" name="email" placeholder="Seu endereço de e-mail..." required="">
+                                            <input type="submit" value="ENVIAR">
                                         </div>
                                         <div id="subscribe-msg"></div>
                                     </form>
@@ -71,72 +71,92 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
-                    <div class="widget widget_text  clearfix">
-                        <h3 class="widget-title">About Business</h3>
+                    <div class="widget widget_text clearfix">
+                        <h3 class="widget-title">Sobre o INFOSI - Recursos Humanos</h3>
                         <div class="textwidget widget-text">
-                            As a business consultant, we focus on delivering quantifiable results for our customers, based on a well tested methodology and solid experience.
+                            O INFOSI é o Instituto Nacional de Fomento da Sociedade da Informação, dedicado a impulsionar a inovação e a inclusão digital em nosso país.
+                            <br> Na área de Recursos Humanos, o INFOSI RH valoriza o capital humano por meio do desenvolvimento contínuo, atraindo e retendo talentos para promover um ambiente dinâmico e preparado para os desafios da transformação digital.
+                            
                             <br><br>
                             <div class="social-icons circle social-hover">
                                 <ul class="list-inline">
-                                    <li class="social-facebook"><a class="tooltip-top" target="_blank" href="#" data-tooltip="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                    <li class="social-twitter"><a class="tooltip-top" target="_blank" href="#" data-tooltip="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                    <li class="social-flickr"><a class=" tooltip-top" target="_blank" href="#" data-tooltip="flickr"><i class="fa fa-flickr" aria-hidden="true"></i></a></li>
-                                    <li class="social-linkedin"><a class=" tooltip-top" target="_blank" href="#" data-tooltip="LinkedIn"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                    <li class="social-facebook">
+                                        <a class="tooltip-top" target="_blank" href="#" data-tooltip="Facebook">
+                                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-twitter">
+                                        <a class="tooltip-top" target="_blank" href="#" data-tooltip="Twitter">
+                                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-flickr">
+                                        <a class="tooltip-top" target="_blank" href="#" data-tooltip="Flickr">
+                                            <i class="fa fa-flickr" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-linkedin">
+                                        <a class="tooltip-top" target="_blank" href="#" data-tooltip="LinkedIn">
+                                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                             <br>
                             <div class="mb-20">
-                                <a class="ttm-btn ttm-btn-size-xs ttm-btn-shape-square ttm-btn-style-fill ttm-btn-bgcolor-skincolor ttm-btn-color-white" href="#">READ MORE</a>
+                                <a class="ttm-btn ttm-btn-size-xs ttm-btn-shape-square ttm-btn-style-fill ttm-btn-bgcolor-skincolor ttm-btn-color-white" href="#">LEIA MAIS</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
                     <div class="widget widget_nav_menu clearfix">
-                       <h3 class="widget-title">Quick Links</h3>
+                        <h3 class="widget-title">Links Rápidos</h3>
                         <ul id="menu-footer-services">
-                            <li><a href="#">Make Appointment</a></li>
-                            <li><a href="#">Customer Services</a></li>
-                            <li><a href="#">Department Services</a></li>
-                            <li><a href="#">About Company</a></li>
-                            <li><a href="#">Our Case Studies</a></li>
-                            <li><a href="#">Free Consultation</a></li>
-                            <li><a href="#">Meet Our Experts</a></li>
+                            <li><a href="#">Departamentos INFOSI</a></li>
+                            <li><a href="#">Sobre a Empresa</a></li>
+                            <li><a href="#">Nossos Estudos de Caso</a></li>
+                            <li><a href="#">Conheça Nossos Especialistas</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
                     <div class="widget widget_text clearfix">
-                        <h3 class="widget-title">Latest News</h3>
+                        <h3 class="widget-title">Últimas Notícias</h3>
                         <ul class="widget-post ttm-recent-post-list">
                             <li>
-                                <a href="single-blog.html"><img src="images/blog/01.jpg" alt="post-img"></a>
-                                <span class="post-date">May 01, 2019</span>
-                                <a href="single-blog.html">Successful Growth In Business 2018</a>
+                                <a href="single-blog.html">
+                                    <img src="{{ asset('frontend/images/blog/01.jpg') }}" alt="post-img">
+                                </a>
+                                <span class="post-date">01 de Maio, 2019</span>
+                                <a href="single-blog.html">Crescimento Bem Sucedido em Negócios 2018</a>
                             </li>
                             <li>
-                                <a href="single-blog.html"><img src="images/blog/02.jpg" alt="post-img"></a>
-                                <span class="post-date">May 03, 2019</span>
-                                <a href="single-blog.html">Achieving Best Business Awards</a>
+                                <a href="single-blog.html">
+                                    <img src="{{ asset('frontend/images/blog/02.jpg') }}" alt="post-img">
+                                </a>
+                                <span class="post-date">03 de Maio, 2019</span>
+                                <a href="single-blog.html">Conquistando os Melhores Prêmios Empresariais</a>
                             </li>
                             <li>
-                                <a href="single-blog.html"><img src="images/blog/03.jpg" alt="post-img"></a>
-                                <span class="post-date">May 05, 2019</span>
-                                <a href="single-blog.html">Seminar for Best Marketing Strategy</a>
+                                <a href="single-blog.html">
+                                    <img src="{{ asset('frontend/images/blog/03.jpg') }}" alt="post-img">
+                                </a>
+                                <span class="post-date">05 de Maio, 2019</span>
+                                <a href="single-blog.html">Seminário para a Melhor Estratégia de Marketing</a>
                             </li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
                     <div class="widget flicker_widget clearfix">
-                       <h3 class="widget-title">Our Branches</h3>
-                       <div class="textwidget widget-text">
-                            <img src="images/map-footer.png" class="img-fluid" alt="map-footer">
-                            <br>
-                            <br>
+                        <h3 class="widget-title">Nossas Filiais</h3>
+                        <div class="textwidget widget-text">
+                            <img src="{{ asset('frontend/images/map-footer.png') }}" class="img-fluid" alt="map-footer">
+                            <br><br>
                             <ul class="ttm-our-location-list">
-                                <li><i class="fa fa-map-marker"></i>Australia: 1234  567 890</li>
-                                <li><i class="fa fa-map-marker"></i>Ontario: 1234  567 890</li>
+                                <li><i class="fa fa-map-marker"></i>Austrália: 1234 567 890</li>
+                                <li><i class="fa fa-map-marker"></i>Ontário: 1234 567 890</li>
                             </ul>
                         </div>
                     </div>
@@ -148,19 +168,19 @@
         <div class="container">
             <div class="row copyright">
                 <div class="col-md-12">
-                    <div class="">
-                        <span>Copyright © 2019&nbsp;<a href="#">Fondex</a>. All rights reserved.</span>
+                    <div>
+                        <span>Copyright © 2019&nbsp;<a href="#">Fondex</a>. Todos os direitos reservados.</span>
                     </div>
                 </div>
                 <div class="col-md-12">
                     <ul id="menu-footer-menu" class="footer-nav-menu">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Sobre Nós</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Privacidade</a></li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
 </footer>
-<!--footer end-->
+<!-- Fim do footer -->
